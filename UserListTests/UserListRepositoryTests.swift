a3717b2390a11ead4f06a112c9de78a4d6ed108f
Jean-hugues <jhberkelio@gmail.com>
2025-05-18 18:21:06 +0200

    Ajout des tests

--- UserListTests/UserListRepositoryTests.swift
+++ UserListTests/UserListRepositoryTests.swift
@@ -1,23 +1,41 @@
 //
-//  UserListRepositoryTests.swift
-//  UserListTests
+//  UserListRepositoryTests 2.swift
+//  UserList
 //
-//  Created by Jean-Hugues on 29/03/2025.
+//  Created by Jean-Hugues on 18/05/2025.
 //
+
 
 import XCTest
 @testable import UserList
 
 final class UserListRepositoryTests: XCTestCase {
-    // Happy path test case
+    
+    var viewModel: UserListViewModel!
+    var mockData: MockData = MockData()
+    var repository: UserListRepository!
+    
+    override func setUp() {
+        super.setUp()
+        self.mockData.validResponse = true
+        self.repository = UserListRepository(executeDataRequest: mockData.executeRequest)
+        self.viewModel = UserListViewModel(repository: self.repository)
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        viewModel = nil
+        repository = nil
+    }
+    
+   // Happy path test case
     func testFetchUsersSuccess() async throws {
         // Given
-        let repository = UserListRepository(executeDataRequest: mockExecuteDataRequest)
         let quantity = 2
         
         // When
-        let users = try await repository.fetchUsers(quantity: quantity)
-        
+        let users = try await self.repository.fetchUsers(quantity: quantity)
+
         // Then
         XCTAssertEqual(users.count, quantity)
         XCTAssertEqual(users[0].name.first, "John")
@@ -31,79 +49,44 @@ final class UserListRepositoryTests: XCTestCase {
         XCTAssertEqual(users[1].picture.medium, "https://example.com/medium.jpg")
     }
     
-    // Unhappy path test case: Invalid JSON response
-    func testFetchUsersInvalidJSONResponse() async throws {
-        // Given
-        let invalidJSONData = "invalid JSON".data(using: .utf8)!
-        let invalidJSONResponse = HTTPURLResponse(
-            url: URL(string: "https://example.com")!,
-            statusCode: 200,
-            httpVersion: nil,
-            headerFields: nil
-        )!
+    func testFetchUsers_shouldHandleRepositoryError() async {
+        // GIVEN
+        mockData.validResponse = false // simule une mauvaise réponse
 
-        let mockExecuteDataRequest: (URLRequest) async throws -> (Data, URLResponse) = { _ in
-            return (invalidJSONData, invalidJSONResponse)
-        }
-        
-        let repository = UserListRepository(executeDataRequest: mockExecuteDataRequest)
-        
-        // When
-        do {
-            _ = try await repository.fetchUsers(quantity: 2)
-            XCTFail("Response should fail")
-        } catch {
-            // Then
-            XCTAssertTrue(error is DecodingError)
-        }
-    }
-}
+        // WHEN
+        await self.viewModel.fetchUsers()
 
-private extension UserListRepositoryTests {
-    // Define a mock for executeDataRequest that returns predefined data
-    func mockExecuteDataRequest(_ request: URLRequest) async throws -> (Data, URLResponse) {
-        // Create mock data with a sample JSON response
-        let sampleJSON = """
-            {
-                "results": [
-                    {
-                        "name": {
-                            "title": "Mr",
-                            "first": "John",
-                            "last": "Doe"
-                        },
-                        "dob": {
-                            "date": "1990-01-01",
-                            "age": 31
-                        },
-                        "picture": {
-                            "large": "https://example.com/large.jpg",
-                            "medium": "https://example.com/medium.jpg",
-                            "thumbnail": "https://example.com/thumbnail.jpg"
-                        }
-                    },
-                    {
-                        "name": {
-                            "title": "Ms",
-                            "first": "Jane",
-                            "last": "Smith"
-                        },
-                        "dob": {
-                            "date": "1995-02-15",
-                            "age": 26
-                        },
-                        "picture": {
-                            "large": "https://example.com/large.jpg",
-                            "medium": "https://example.com/medium.jpg",
-                            "thumbnail": "https://example.com/thumbnail.jpg"
-                        }
-                    }
-                ]
-            }
-        """
-        
-        let data = sampleJSON.data(using: .utf8)!
-        let response = HTTPURLResponse(url: request.url!, statusCode: 200, httpVersion: nil, headerFields: nil)!
-        return (data, response)
+        // THEN
+        XCTAssertTrue(viewModel.users.isEmpty)
+        XCTAssertNotNil(viewModel.errorMessage)
     }
+
+    
+//
+//    // Unhappy path test case: Invalid JSON response
+//    func testFetchUsersInvalidJSONResponse() async throws {
+//        // Given
+//        let invalidJSONData = "invalid JSON".data(using: .utf8)!
+//        let invalidJSONResponse = HTTPURLResponse(
+//            url: URL(string: "https://example.com")!,
+//            statusCode: 200,
+//            httpVersion: nil,
+//            headerFields: nil
+//        )!
+//
+//        let mockExecuteDataRequest: (URLRequest) async throws -> (Data, URLResponse) = { _ in
+//            return (invalidJSONData, invalidJSONResponse)
+//        }
+//        
+//        let repository = UserListRepository(executeDataRequest: mockExecuteDataRequest)
+//        
+//        // When
+//        do {
+//            _ = try await repository.fetchUsers(quantity: 2)
+//            XCTFail("Response should fail")
+//        } catch {
+//            // Then
+//            XCTAssertTrue(error is DecodingError)
+//        }
+//    }
 }
--- UserListTests/UserListViewModelTests.swift
+++ UserListTests/UserListViewModelTests.swift
@@ -1,52 +1,186 @@
 //
 //  UserListViewModelTests.swift
-//  UserListTests
+//  UserList
 //
-//  Created by Jean-Hugues on 29/03/2025.
+//  Created by Jean-Hugues on 18/05/2025.
 //
+
 
 import XCTest
 @testable import UserList
 
 final class UserListViewModelTests: XCTestCase {
+    var viewModel: UserListViewModel!
+    var mockData: MockData = MockData()
+    var repository: UserListRepository!
 
-    // Teste si la récupération d'utilisateurs fonctionne correctement avec le FakeRepository
-    func testFetchUsersSuccess() async {
-        // Given : Initialisation d'un fake repository avec des utilisateurs factices
-        let fakeRepository = FakeUserListRepository()
-        fakeRepository.fakeUsers = [
-            User(name: "John", dob: "1990", picture: "url1"),
-            User(name: "Jane", dob: "1995", picture: "url2")
-        ]
-        
-        // On initialise le ViewModel avec notre Fake Repository
-        let viewModel = UserListViewModel(repository: fakeRepository)
-        
-        // When : On appelle la méthode pour récupérer les utilisateurs
+    override func setUp() {
+        super.setUp()
+        mockData.validResponse = true
+        repository = UserListRepository(executeDataRequest: mockData.executeRequest)
+        viewModel = UserListViewModel(repository: repository)
+    }
+
+    override func tearDown() {
+        viewModel = nil
+        repository = nil
+        super.tearDown()
+    }
+
+    func testShouldLoardMoreData() async {
+        //Given
         await viewModel.fetchUsers()
-        
-        // Then : On vérifie que les utilisateurs ont bien été ajoutés au ViewModel
-        XCTAssertEqual(viewModel.users.count, 2, "Le nombre d'utilisateurs devrait être 2.")
-        XCTAssertEqual(viewModel.users[0].name, "John", "Le premier utilisateur devrait être John.")
-        XCTAssertEqual(viewModel.users[1].name, "Jane", "Le second utilisateur devrait être Jane.")
+
+        let currentUser = viewModel.users.last!
+
+        //When
+        let shouldLoadMoreData = viewModel.shouldLoadMoreData(currentItem: currentUser)
+
+        //Then
+        XCTAssertTrue(shouldLoadMoreData)
     }
 
-    // Teste si une erreur est bien lancée dans le cas où le repository échoue
-    func testFetchUsersFailure() async {
-        // Given : Initialisation du fake repository avec un comportement d'échec
-        let fakeRepository = FakeUserListRepository()
-        fakeRepository.shouldFail = true
-        
-        // On initialise le ViewModel avec notre Fake Repository
-        let viewModel = UserListViewModel(repository: fakeRepository)
-        
-        // When : On appelle la méthode pour récupérer les utilisateurs
-        do {
-            try await viewModel.fetchUsers()
-            XCTFail("L'appel aurait dû échouer")
-        } catch {
-            // Then : On vérifie que l'erreur se produit
-            XCTAssertTrue(error is NSError, "Une erreur devrait être lancée.")
-        }
+    func testShouldNotLoardMoreData() async {
+        //Given
+        await viewModel.fetchUsers()
+
+        let currentUser = viewModel.users.first!
+
+        //When
+        let shouldLoadMoreData = viewModel.shouldLoadMoreData(currentItem: currentUser)
+
+        //Then
+        XCTAssertFalse(shouldLoadMoreData)
     }
+
+    func testReloadUsersSuccess() async {
+        //Given
+        await viewModel.fetchUsers()
+
+        let initialUsers = viewModel.users
+        //When
+        await viewModel.reloadUsers()
+
+        //Then
+        for user in viewModel.users {
+            XCTAssertFalse(initialUsers.contains(where: { $0.id == user.id }))
+        }
+
+
+    }
+
+//    // Teste si la récupération d'utilisateurs fonctionne correctement avec le FakeRepository
+//    func testFetchUsersSuccess() async {
+//        // Given : Initialisation d'un fake repository avec des utilisateurs factices
+//        let fakeRepository = FakeUserListRepository()
+//        fakeRepository.fakeUsers = [
+//            User(user: UserListResponse.User(
+//                name: .init(title: "Mr", first: "John", last: "Doe"),
+//                dob: .init(date: "1990-01-01", age: 31),
+//                picture: .init(large: "url1", medium: "", thumbnail: "")
+//            )),
+//            User(user: UserListResponse.User(
+//                name: .init(title: "Ms", first: "Jane", last: "Smith"),
+//                dob: .init(date: "1995-02-15", age: 26),
+//                picture: .init(large: "url2", medium: "", thumbnail: "")
+//            ))
+//        ]
+//        
+//        // On initialise le ViewModel avec notre Fake Repository
+//        let viewModel = UserListViewModel(repository: fakeRepository)
+//        
+//        // When : On appelle la méthode pour récupérer les utilisateurs
+//        await viewModel.fetchUsers()
+//        
+//        // Then : On vérifie que les utilisateurs ont bien été ajoutés au ViewModel
+//        let user = viewModel.users[0]
+//        XCTAssertEqual(user.name.first, "John")
+//        XCTAssertEqual(user.name.last, "Doe")
+//        XCTAssertEqual(user.dob.date, "1990-01-01")
+//        XCTAssertEqual(user.dob.age, 31)
+//        XCTAssertEqual(user.picture.large, "url1") // ou .thumbnail ou .medium selon le cas
+//
+//    }
+//
+//    // Teste si une erreur est bien lancée dans le cas où le repository échoue
+//    func testFetchUsersFailure() async {
+//        // Given : Initialisation du fake repository avec un comportement d'échec
+//        let fakeRepository = FakeUserListRepository()
+//        fakeRepository.shouldFail = true
+//        
+//        // On initialise le ViewModel avec notre Fake Repository
+//        let viewModel = UserListViewModel(repository: fakeRepository)
+//        
+//        // When : On appelle la méthode pour récupérer les utilisateurs
+//        do {
+//            await viewModel.fetchUsers()
+//            XCTAssertEqual(viewModel.errorMessage, "Une erreur est survenue lors du chargement.")
+//        }
+//    }
+//    
+//    func testReloadUsers_ShouldClearUsersAndFetchAgain() async {
+//        let fakeRepository = FakeUserListRepository()
+//        fakeRepository.fakeUsers = [
+//            makeFakeUser(first: "Alice"),
+//            makeFakeUser(first: "Bob")
+//        ]
+//
+//        let viewModel = UserListViewModel(repository: fakeRepository)
+//
+//        await viewModel.fetchUsers()
+//        XCTAssertEqual(viewModel.users.count, 2)
+//
+//        // Change les données pour simuler un "rechargement"
+//        fakeRepository.fakeUsers = [
+//            makeFakeUser(first: "Charlie")
+//        ]
+//
+//        await viewModel.reloadUsers()
+//
+//        XCTAssertEqual(viewModel.users.count, 1)
+//        XCTAssertEqual(viewModel.users[0].name.first, "Charlie")
+//    }
+//
+//    private func makeFakeUser(first: String) -> User {
+//        User(user: UserListResponse.User(
+//            name: .init(title: "Mr", first: first, last: "Test"),
+//            dob: .init(date: "1990-01-01", age: 30),
+//            picture: .init(large: "url", medium: "url", thumbnail: "url")
+//        ))
+//    }
+//
+//    func testShouldLoadMoreData_WhenOnLastItemAndNotLoading_ShouldReturnTrue() {
+//        let user = makeFakeUser(first: "Foo")
+//        let viewModel = UserListViewModel(repository: FakeUserListRepository())
+//        viewModel.users = [user]
+//        viewModel.isLoading = false
+//
+//        let result = viewModel.shouldLoadMoreData(currentItem: user)
+//        XCTAssertTrue(result)
+//    }
+//
+//    func testShouldLoadMoreData_WhenNotOnLastItem_ShouldReturnFalse() {
+//        let first = makeFakeUser(first: "A")
+//        let second = makeFakeUser(first: "B")
+//        let viewModel = UserListViewModel(repository: FakeUserListRepository())
+//        viewModel.users = [first, second]
+//        viewModel.isLoading = false
+//
+//        let result = viewModel.shouldLoadMoreData(currentItem: first)
+//        XCTAssertFalse(result)
+//    }
+//
+//    func testShouldLoadMoreData_WhenLoading_ShouldReturnFalse() {
+//        let user = makeFakeUser(first: "C")
+//        let viewModel = UserListViewModel(repository: FakeUserListRepository())
+//        viewModel.users = [user]
+//        viewModel.isLoading = true
+//
+//        let result = viewModel.shouldLoadMoreData(currentItem: user)
+//        XCTAssertFalse(result)
+//    }
+
+
+
+    
 }
